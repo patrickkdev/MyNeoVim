4d39caecc9b6f086d23765658e53de0d617cc667
patrickkdev <patrick8q@gmail.com>
2024-08-09 20:46:43 -0300

    Remove some language servers

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { 'gopls', 'ccls', 'cmake', 'tsserver', 'templ', 'eslint', "typescript", "typescriptreact", "typescript.tsx" }
+local servers = { 'gopls', 'ccls', 'cmake', 'tsserver', 'templ', 'eslint' }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- Keep diagnostic message on insert mode
