987c98977042f33f23bfd084d5aded9bf5e4c791
patrickkdev <patrick8q@gmail.com>
2024-08-11 19:56:25 -0300

    Update

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,7 +5,7 @@
 local M = {}
 
 M.base46 = {
-	theme = "chadracula-evondev",
+	theme = "dark_horizon",
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
